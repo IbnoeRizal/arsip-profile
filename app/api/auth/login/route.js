ed03ca138bb3eefce08c29c9b5cbdef311daf413
IbnoeRizal <ibnurizal849@gmail.com>
2026-01-27 22:15:41 +0700

    change authorization code from header to cookie

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -1,10 +1,9 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
-import { Role } from "@prisma/client";
 import { flaterr, USER_LOGIN } from "@/lib/authschema";
 import { st2xx, st4xx, st5xx } from "@/lib/responseCode";
 import { verifyhashpass } from "@/lib/hashpass";
-import { getToken } from "@/lib/auth";
+import { getToken,cookie_name } from "@/lib/auth";
 import { prismaError } from "@/lib/prismaErrorResponse";
 
 /**
@@ -20,7 +19,7 @@ export async function POST(request) {
         if(!validate.success)
             return NextResponse.json({data:flaterr(validate.error)},{status:st4xx.badRequest});
 
-        const user = await prisma.user.findUniqueOrThrow({
+        const user = await prisma.user.findUnique({
             where:{email:validate.data.email},
             select:{
                 id:true,
@@ -29,10 +28,29 @@ export async function POST(request) {
             }
         });
 
-        return await verifyhashpass(validate.data.password, user.password)? 
-            NextResponse.json({data: await getToken({id:user.id, role:user.role})},{status:st2xx.ok}):
-            NextResponse.json({data: "data tidak ditemukan"},{status:st4xx.notFound});
+        if(!user)
+            return NextResponse.json({data: "email atau password salah"},{status:st4xx.unauthorized});
 
+        const verified = await verifyhashpass(validate.data.password, user.password);
+        
+        if(!verified)
+            return NextResponse.json({data: "email atau password salah"},{status:st4xx.unauthorized});
+        
+
+        const res = NextResponse.json({data:"login success"},{status:st2xx.ok})
+        const {token,maxAge} = await getToken({id:user.id, role:user.role},1)
+        res.cookies.set({
+            name:cookie_name,
+            value: token,
+            maxAge: maxAge,
+            path: "/",
+            sameSite: "strict",
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production"
+        });
+
+        return res;
+        
     }catch(e){
         const knownErr = prismaError(e);
         if(knownErr)
