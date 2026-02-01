63451280134249415e63aebdbf9b65f1f087bdef
IbnoeRizal <ibnurizal849@gmail.com>
2026-02-01 10:41:20 +0700

    add size w,h props

--- components/button.jsx
+++ components/button.jsx
@@ -1,4 +1,4 @@
 "use client"
-export default function ThemeButton({fun, text}){
-    return <button onClick={fun} className="flex dark:bg-blue-600 bg-red-600 rounded-md p-1 hover:scale-101 active:scale-99" >{text}</button>
+export default function ThemeButton({fun, text, width, height}){
+    return <button onClick={fun} className="flex dark:bg-blue-600 bg-red-600 rounded-md p-1 hover:scale-101 active:scale-99" style={{width, height}}>{text}</button>
 }
