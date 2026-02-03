c3a370e9242b4cfbfaebfc2cf55a03586a4b6c01
IbnoeRizal <ibnurizal849@gmail.com>
2026-02-03 09:48:35 +0700

    fix layouting page.jsx

--- app/karyawan/[id]/page.jsx
+++ app/karyawan/[id]/page.jsx
@@ -1,8 +1,10 @@
-import Login from "@/components/form/login";
+import {LoginWrapper} from "@/components/form/login";
 import { getUserFromCookie } from "@/lib/auth";
 import prisma from "@/lib/prisma";
 import GetUserInfo from "./user.id";
 import { notFound } from "next/navigation";
+import Link from "next/link";
+import DriveItems from "@/components/driveItem";
 
 export default async function Page({ params }) {
   const { id } = await params;
@@ -13,10 +15,8 @@ export default async function Page({ params }) {
       select: { 
         id: true, 
         driveObj:{
-          where:{
-            category:"POFILEPIC"
-          },
           select:{
+            id: true,
             link:true
           }
         }
@@ -32,9 +32,16 @@ export default async function Page({ params }) {
   return (
     <div className="flex flex-col gap-5 bg-inherit justify-center mt-16 w-full items-center">
       <GetUserInfo key={id} id={id}/>
-
-      <div></div>
-      {!user &&<Login/>}
+      {user && user.id === id && 
+        <section className="container flex flex-col justify-center items-center gap-4"> 
+          <h1>Daftar File</h1>
+          <div className="container self-center sm:grid sm:grid-cols-3 sm:place-content-center sm:place-items-center-stretch flex justify-center items-center flex-wrap gap-4 border border-dotted sm:p-5 rounded-md *:hover:brightness-90">
+            {
+              hasUser?.driveObj.map((obj,i) => (<DriveItems key={obj.id} {...obj}/>))
+            }
+          </div>
+        </section>
+      }
     </div>
 
   );
