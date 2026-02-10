0407e03aebc429038bd689dab28aee94baac3573
IbnoeRizal <ibnurizal849@gmail.com>
2026-02-10 08:35:48 +0700

    add drive link handler for picture in order to show the image

--- components/profilepic.jsx
+++ components/profilepic.jsx
@@ -3,12 +3,24 @@ import { useEffect, useRef, useState } from "react";
 import { UserCircle2Icon } from "lucide-react"
 import handleParseResponse from "@/lib/fetch/handlefetch";
 
+const colorPallet = {
+    ERROR : {
+        stroke : "stroke-red-900"
+    },
+
+    OK : {
+        stroke : "dark:stroke-blue-300"
+    }
+}
+
 
 export default function ProfilePic({id,w=300,h=300,fun}){
     const [data, setData] = useState({
         text: "",
         code: 0
     });
+
+    const [isError, setError] = useState("Ok");
 
     const controller = useRef(null);
 
@@ -25,10 +37,27 @@ export default function ProfilePic({id,w=300,h=300,fun}){
                 if(!mounted) return;
                 
                 fun?.(result?.data.id ?? "");
+                
+                const drivePicture = (()=>{
+                    //get id file from googledrive file
+                    let original = String(result?.data.link);
+                    if(!(original.includes("drive.google.com")))
+                        return null;
+
+                    const url = new URL(original);
+                    const parts = url.pathname.split("/");
+                    const d_index = parts.indexOf("d");
+                    return d_index !== -1 ? `https://drive.google.com/thumbnail?id=${parts[d_index + 1]}` : null;
+                })();
+                if(drivePicture)
+                    console.log(drivePicture);
+
                 setData({
-                    text: result?.data.link ?? "",
+                    text: drivePicture ?? result?.data.link ?? "",
                     code: response.status,
                 })     
+
+                setError(response.statusText)
             }catch(e){
                 if(e.name !== "AbortError" && process.env.NODE_ENV === "development")
                     console.error(e);
@@ -43,12 +72,22 @@ export default function ProfilePic({id,w=300,h=300,fun}){
         };
     },[id,fun]);
 
-  
+    const c = colorPallet[isError]?.stroke ?? colorPallet["ERROR"].stroke;
+    
     return(
         <div className="rounded-full overflow-hidden shrink-0 ring-1 dark:ring-blue-300 ring-black/10" style={{width:w, height:h}}>
             {data.code/100 < 3 && data.text !== ""?
-                <img src={data.text} className="w-full h-full object-cover" alt="profile" onError={()=>setData(prev=>({...prev,text:""}))}></img>: 
-                <UserCircle2Icon className="mask-circle dark:stroke-blue-300 w-full" style={{width:w, height:h}}/>
+                <img 
+                    src={data.text} 
+                    className="w-full h-full object-cover" 
+                    alt="profile" 
+                    onError={()=>{
+                            setData(prev=>({...prev,text:""}))
+                            setError("ERROR")
+                        }}
+                    loading="lazy"
+                ></img>: 
+                <UserCircle2Icon className={`mask-circle ${c} w-full`} style={{width:w, height:h}}/>
             }
         </div>
     )  
