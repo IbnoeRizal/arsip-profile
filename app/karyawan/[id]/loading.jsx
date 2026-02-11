1267db6269af47250c9f7b06ea4aae26c4bdd074
IbnoeRizal <ibnurizal849@gmail.com>
2026-02-11 20:44:32 +0700

    delete loading, since loading is propagated to all next path

--- app/karyawan/[id]/loading.jsx
+++
@@ -1,9 +0,0 @@
-import Loader from "@/components/loading";
-
-export default function Loading() {
-  return (
-    <div className="fixed inset-0 flex items-center justify-center">
-      <Loader />
-    </div>
-  );
-}
