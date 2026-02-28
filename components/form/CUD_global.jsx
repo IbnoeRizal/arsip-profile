382f110c31def7b21f2f897be542758161480dc5
IbnoeRizal <ibnurizal849@gmail.com>
2026-02-28 19:14:04 +0700

    add global export for CUD component and config

---
+++ components/form/CUD_global.jsx
@@ -0,0 +1,10 @@
+// export global semua CUD form dengan konfigurasinya
+
+export * from "./driveObjCUD";
+export * from "./jabatanCUD";
+export * from "./kelasCUD";
+export * from "./mapelCUD";
+export * from "./mengajarCUD";
+export * from "./misiCUD";
+export * from "./userCUD";
+export * from "./visiCUD";
