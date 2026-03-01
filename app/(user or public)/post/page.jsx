31da4d29ac70ae2b1c6abc0421454262a7993458
IbnoeRizal <ibnurizal849@gmail.com>
2026-03-02 06:49:50 +0700

    add save and load handler

--- app/(user or public)/post/page.jsx
+++ app/(user or public)/post/page.jsx
@@ -1,8 +1,14 @@
 'use client'
 import { ForwardRefEditor } from "@/components/editor/ForwardRefEditor";
 import { useKeyboardPresece } from "@/customHooks/keyboard_detector";
+import { useEffect, useRef, useState } from "react";
+import { 
+    Save,
+    Send,
+    Download 
+} from "lucide-react";
 
-const keyword = ["kegunaan","contoh pemakaian"]
+const keyword = ["kegunaan","contoh pemakaian"];
 
 const shortcut = Object.freeze({
     ["#"] : Object.freeze({
@@ -33,6 +39,46 @@ const shortcut = Object.freeze({
 
 export default function page(){
     const hasKeyboard = useKeyboardPresece();
+    const editorRef = useRef(null);
+    const name = "finalMd";
+
+    const setEditorRef = (instance) => {
+        if (instance) {
+            editorRef.current = instance;
+            load();
+        } else {
+            if (editorRef.current) {
+                save(); // autosave sebelum hilang
+            }
+            editorRef.current = null;
+        }
+    };
+
+    function send(){
+        let finalMd;
+        if(!editorRef.current || !(finalMd = editorRef.current.getMarkdown().trim())){
+            return alert("system error, Editor or Content is missing");
+        }
+
+        if(window.confirm("Data akan dikirimkan, apakah anda yakin")){
+            alert(finalMd);
+        }
+    };
+
+    function save(){
+        if(!editorRef.current) return;
+
+        const finalMd = editorRef.current.getMarkdown();
+        sessionStorage.setItem(name, finalMd);
+    };
+
+    function load(){
+        if(!editorRef.current) return;
+        const data = sessionStorage.getItem(name);
+        if(!data) return;
+        editorRef.current.setMarkdown(data);
+    }
+
     return(
     <>
         <section className={`mx-4 max-w-full flex flex-col justify-around items-center`}>
@@ -45,7 +91,10 @@ export default function page(){
         <section className="mt-20 mx-4 max-w-full flex flex-col justify-around items-center">
             <h2 className="mb-4 text-3xl text-left w-full font-bold" >Editor</h2>
             <div className="max-w-full bg-foreground/10 rounded-md self-stretch items-stretch flex justify-stretch *:w-full">
-                <ForwardRefEditor onChange={(e)=>console.log(e)} />
+                <ForwardRefEditor ref={setEditorRef}/>
+            </div>
+            <div className="flex flex-row self-end justify-center items-center *:grow-0 *:shrink-0 gap-2 mt-2">
+                <button type="button" id="send" onClick={send} className="bg-foreground/10 p-2 rounded-md flex flex-row justify-evenly gap-2 items-center active:bg-background/10">Send <Send className="dark:stroke-blue-500 stroke-red-500"/></button>
             </div>
         </section>
         <section className={`mt-20 mx-4 max-w-full flex flex-col justify-around items-centers not-first:gap-10 ${!hasKeyboard ? `invisible`:``}`}>
